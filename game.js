7c8b7431003bc9f4b09482a0fd4f7550f3f644af
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-25 15:21:04 -0500

    adds variable to store sequence

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 buttonColours = ["red", "blue", "green", "yellow"];
+gamePattern = [];
 function nextSequence() {
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
