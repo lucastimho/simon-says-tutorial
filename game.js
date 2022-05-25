154905559f755abccb89f1328e0f4c49ae1f186f
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-25 16:50:03 -0500

    refactors audio into new function

--- game.js
+++ game.js
@@ -6,10 +6,14 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio(`./sounds/${randomChosenColour}.mp3`);
-    audio.play();
+    playSound(randomChosenColour);
 }
 $(".btn").click(function() {
     var userChosenColour = this.id;
+    playSound(userChosenColour);
     userClickedPattern.push(userChosenColour);
 })
+function playSound(name) {
+    var audio = new Audio(`./sounds/${name}`);
+    audio.play();
+}
