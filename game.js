fe89badff7828ad654cfc22692fc2c3ac0627b62
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-25 16:09:25 -0500

    changes function to animate and play audio

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
-buttonColours = ["red", "blue", "green", "yellow"];
-gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 function nextSequence() {
-    randomNumber = Math.floor(Math.random() * 4);
-    randomChosenColour = buttonColours[randomNumber];
-    return randomChosenColour;
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio(`./sounds/${randomChosenColour}.mp3`);
+    audio.play();
 }
