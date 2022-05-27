872ee9df0a48d451e72bcf6b92869ac6875f2c74
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-26 23:51:12 -0500

    adds timeout to remove pressed class

--- game.js
+++ game.js
@@ -20,4 +20,7 @@ function playSound(name) {
 }
 function animatePress(currentColour) {
     $(`#${currentColour}`).addClass("pressed");
+    setTimeout(function() {
+        $(`#${currentColour}`).removeClass("pressed");
+    }, 100)
 }
