40263ed9f7417ed655cead1f14957b5f26c0084a
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-30 12:24:35 -0500

    changes next sequence function to play entire sequence

--- game.js
+++ game.js
@@ -7,8 +7,7 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+  playEntireSequence();
 }
 function playEntireSequence() {
   for (let i = 0; i < gamePattern.length; i++) {
